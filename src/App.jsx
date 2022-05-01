60c1544cb713e114abdb22a27a18a912d6a47c0b
Vishal Vivare <96101110+vishalvivare@users.noreply.github.com>
2022-05-01 16:57:20 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import {useState,useEffect} from "react"
+import './App.css'
+
+const App = () => {
+  const [time, setTime] =useState(0)
+  const [timerOn, setTimerOn] =useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 6);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
+  return (
+    <div className="Timers">
+      <p>Timer</p>
+      <div id="display">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+
+      <div id="buttons">
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
+        <div id="buttonFlex">
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTime(0)}>Reset</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default App;
